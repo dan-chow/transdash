2df7f5f3eff7e55bf65b53aa9d07778a8caf9467
dan-chow <nju.zhoulei@gmail.com>
2018-03-01 15:53:12 +0800

    MPD getter and parser half done

--- DashProxy/src/HomeController.java
+++ DashProxy/src/HomeController.java
@@ -5,22 +5,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.gson.Gson;
-
 @WebServlet("/hello")
 public class HomeController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		String name = request.getParameter("name");
-		int age = Integer.parseInt(request.getParameter("age"));
-		Person person = new Person(name, age);
 
-		String jsonResult = new Gson().toJson(person);
-
-		response.setContentType("application/json");
-		response.setCharacterEncoding("UTF-8");
-		response.getWriter().write(jsonResult);
 	}
 }
