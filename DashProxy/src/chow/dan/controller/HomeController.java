c0ebb67992dc9197bcaaa286af4acc7597da60a2
dan-chow <nju.zhoulei@gmail.com>
2018-03-06 23:04:10 +0800

    controller transfer file back

--- DashProxy/src/chow/dan/controller/HomeController.java
+++ DashProxy/src/chow/dan/controller/HomeController.java
@@ -2,35 +2,33 @@ package chow.dan.controller;
 
 import java.io.IOException;
 
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.xml.bind.JAXBException;
 
-import chow.dan.bll.ContentManager;
-import chow.dan.bll.MpdManager;
-import chow.dan.bll.SegmentData;
-import chow.dan.bll.SegmentManager;
+import chow.dan.bll.Downloader;
 import chow.dan.common.Content;
 
-@WebServlet("/hello")
+@WebServlet("/proxy")
 public class HomeController extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		// TODO
-		String uri = "http://114.212.84.179:8080/video/result.mpd";
-		Content mpdContent;
-		try {
-			mpdContent = ContentManager.get(uri);
-			// TODO FilenameUtils
-			String baseUri = uri.substring(0, uri.lastIndexOf('/'));
-			SegmentManager.getInstance().put(baseUri, new SegmentData(MpdManager.parseMpd(mpdContent)));
-		} catch (JAXBException e) {
-			e.printStackTrace();
-		}
+		String url = request.getParameter("url");
+		System.out.println(url);
+
+		Content content = Downloader.download(url);
+
+		System.out.println(content.getData().length);
+
+		response.setHeader("Content-Disposition", "filename=" + url);
+		response.setContentLength(content.getData().length);
+		ServletOutputStream os = response.getOutputStream();
+		os.write(content.getData());
+		os.close();
 	}
 }
